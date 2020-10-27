35c0a2e262712bd6074d853624581c37acb0ee32
vagrant <vagrant@myprojects.lh>
2020-10-27 12:00:47 +0000

    Commit

--- functions/apache_vhost_fpm_proxy.pp
+++ functions/apache_vhost_fpm_proxy.pp
@@ -2,9 +2,9 @@
 #   Return rewrite rules used in apache virtual hosts.
 #
 #########################################################################
-function vs_lamp::apache_vhost_fpm_proxy( String $fpmSocket ) {
+function vs_lamp::apache_vhost_fpm_proxy( $fpmSocket ) {
     
-    if ( ! empty( $fpmSocket ) ) {
+    if ( $fpmSocket and ! empty( $fpmSocket ) ) {
         '
             <Proxy "unix:${fpmSocket}|fcgi://php-fpm">
                ProxySet disablereuse=off
