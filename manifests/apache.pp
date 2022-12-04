c308558ef9a2f0bb8554e64da6e2045ddc062149
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-12-04 04:47:15 +0000

    Fix Apache Mod PHP Configuration.

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -67,14 +67,25 @@ class vs_lamp::apache (
         }
     }
     
+    ###################################################################################
     # Setup mod_php
-    ###########################
-    class {'::apache::mod::php':
-        php_version  => "${phpVersion}",
-        path         => "modules/libphp${phpVersion}.so",
-    }->
-    file { "/usr/lib64/httpd/modules/libphp${phpVersion}.so":
-        ensure => 'link',
-        target => "/usr/lib64/httpd/modules/libphp${$phpVersion[0]}.so",
+    ###################################################################################
+    $modPhpVersionLib       = "/usr/lib64/httpd/modules/libphp${$phpVersion[0]}.so"
+    $modPhpVersionLibExists = find_file( $modPhpVersionLib )
+    
+    if ( $modPhpVersionLibExists )  {
+        class {'::apache::mod::php':
+            php_version  => "${phpVersion}",
+            path         => "modules/libphp${phpVersion}.so",
+        }->
+        file { "/usr/lib64/httpd/modules/libphp${phpVersion}.so":
+            ensure => 'link',
+            target => "/usr/lib64/httpd/modules/libphp${$phpVersion[0]}.so",
+        }
+    } else {
+        class {'::apache::mod::php':
+            php_version  => "${phpVersion}",
+            path         => "modules/libphp.so",
+        }
     }
 }
