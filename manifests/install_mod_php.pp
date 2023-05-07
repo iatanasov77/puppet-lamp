50318e076ea8016b36c18b52986b683b1e52b109
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-05-07 10:41:17 +0000

    Fix Configuration For Mod PHP.

--- manifests/install_mod_php.pp
+++ manifests/install_mod_php.pp
@@ -2,11 +2,20 @@ class vs_lamp::install_mod_php (
     String $phpVersion,
 ) {
     if ( $phpVersion ) {
+    
+        if $phpVersion[0] == '7' {
+            $modphpPath = 'modules/libphp7.so'
+        } else {
+            $modphpPath = 'modules/libphp.so'
+        }
+        
         class {'::apache::mod::php':
             php_version  => "${phpVersion}",
             #path         => "modules/libphp${phpVersion[0]}.so",
-            path         => "modules/libphp.so",
+            path         => "${modphpPath}",
         }
+        
+        
         /* SETUP NOT WORK HOW ITS EXPECTED
 	    -> class { '::vs_lamp::setup_mod_php':
 	        phpVersion  => "${phpVersion}",
